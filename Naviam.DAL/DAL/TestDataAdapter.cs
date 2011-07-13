a4d7eba109954def02e4dd3cac7481b1b58cd9e4
Aleksey Yurkevich <BlackTrinitron@gmail.com>
2011-07-13 13:20:08 +0300

    added skeleton of DAL

---
+++ Naviam.DAL/DAL/TestDataAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Npgsql;
+
+namespace Naviam.DAL
+{
+    public class TestDataAdapter
+    {
+        public static void Test()
+        {
+            using (SqlConnectionHolder holder = SqlConnectionHelper.GetConnection(SqlConnectionHelper.ConnectionType.Naviam))
+            {
+                NpgsqlCommand command = new NpgsqlCommand("select * from tst", holder.Connection);
+            }
+
+        }
+    }
+}
