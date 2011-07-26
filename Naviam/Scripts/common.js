6423b9c32c16865520bf979e384e45ab9eb9b059
Aleksey Yurkevich <BlackTrinitron@gmail.com>
2011-07-26 23:22:04 +0300

    blueprint tools moved to root

--- Naviam/Scripts/common.js
+++ Naviam/Scripts/common.js
@@ -1,1 +1,32 @@
 ﻿/// <reference path="jquery-1.6.2.js" />
+jQuery.postErr = function (url, data, callback, type) {
+    // shift arguments if data argument was omited
+    if ($.isFunction(data)) {
+        type = type || callback;
+        callback = data;
+        data = null;
+    }
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        complete: function () {
+//            $("#saving").hide();
+//            $("#loading").hide();
+        },
+        success: callback,
+        dataType: type,
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            if (XMLHttpRequest.status == 500) {
+                var errorObj = JSON.parse(XMLHttpRequest.responseText);
+                showSiteError(errorObj.Text);
+            }
+            else {
+                showSiteError(XMLHttpRequest.responseText);
+            }
+        }
+    });
+}
+function showSiteError(errorText) {
+    alert(errorText);
+}
