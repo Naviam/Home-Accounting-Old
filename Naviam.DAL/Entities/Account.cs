fa3fbd888476e9729ef2302e04aee1a86a2dc453
PavelMironchik <pavel.mironchik@gmail.com>
2011-07-31 22:34:20 +0300

    add entity account

---
+++ Naviam.DAL/Entities/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Naviam.Data
+{
+    [Serializable]
+    public class Account : DbEntity
+    {
+        public enum AccountTypes { Bank=0, WebMoney, Home }
+        const string Bank = "bank";
+        const string WebMoney = "webmoney";
+        const string Home = "home";
+
+        public static AccountTypes GetAccountType(string accTypeStr)
+        {
+            switch (accTypeStr)
+            {
+                case Bank: return AccountTypes.Bank;
+                case WebMoney: return AccountTypes.WebMoney;
+                case Home: return AccountTypes.Home;
+                default: return AccountTypes.Bank;
+            }
+        }
+    }
+}
