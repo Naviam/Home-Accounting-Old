84a1af84bc070268feeea1ddb0e394517533c55f
PavelMironchik <pavel.mironchik@gmail.com>
2011-08-01 11:54:22 +0300

    add categories data adapter

--- Naviam.DAL/Entities/Category.cs
+++ Naviam.DAL/Entities/Category.cs
@@ -17,11 +17,35 @@ namespace Naviam.Data
             Subitems = new List<Category>();
         }
         public Category(SqlDataReader reader) 
-        { 
-         //TODO: read fields
+        {
+            Id = reader["id"] as int?;
+            ParentId = reader["parent_id"] as int?;
+            Name = reader["name"] as string;
+            Subitems = new List<Category>();
         }
 
         public string Name { get; set; }
         public List<Category> Subitems { get; set; }
+        public int? ParentId { get; set; }
+    }
+    
+    [Serializable]
+    public class Categories : List<Category>
+    {
+        public Categories() { }
+        public Categories(SqlDataReader reader) 
+        {
+            while (reader.Read())
+            {
+                Add(new Category(reader));
+            }
+
+            foreach (Category item in FindAll(x => x.ParentId==null))
+            {
+                item.Subitems.AddRange(FindAll(x => x.ParentId == item.Id));
+                this.RemoveAll(x => x.ParentId == item.Id);
+            }
+        }
+    
     }
 }
