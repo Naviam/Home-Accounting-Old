11a076048103a45453652aefddcd5dfc9860d33e
PavelMironchik <pavel.mironchik@gmail.com>
2011-08-01 14:58:08 +0300

    add naviam sql files

---
+++ Database/Schema Objects/Database Level Objects/Storage/Files/naviam_log.sqlfile.sql
@@ -0,0 +1,3 @@
+﻿ALTER DATABASE [$(DatabaseName)]
+    ADD LOG FILE (NAME = [naviam_log], FILENAME = 'c:\Program Files\Microsoft SQL Server\MSSQL10_50.SQLEXPRESS\MSSQL\DATA\naviam_log.ldf', SIZE = 1024 KB, MAXSIZE = 2097152 MB, FILEGROWTH = 10 %);
+
