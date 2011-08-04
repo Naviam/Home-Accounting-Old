b06a818b8bd2fa8d1c43d7852d187f1927b194cf
PavelMironchik <pavel.mironchik@gmail.com>
2011-08-04 16:12:44 +0300

    add company DAL

---
+++ Naviam.DAL/DAL/CompaniesDataAdapter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Naviam.Code;
+using System.Data.SqlClient;
+using Naviam.Data;
+
+namespace Naviam.DAL
+{
+    public class CompaniesDataAdapter
+    {
+        private const string CacheKey = "userCompanies";
+
+        public static IEnumerable<Company> GetCompanies(int? userId) { return GetCompanies(userId, false); }
+        public static IEnumerable<Company> GetCompanies(int? userId, bool forceSqlLoad)
+        {
+            List<Company> res = CacheWrapper.GetList<Company>(CacheKey, userId);
+            if (res == null || forceSqlLoad)
+            {
+                using (SqlConnectionHolder holder = SqlConnectionHelper.GetConnection(SqlConnectionHelper.ConnectionType.Naviam))
+                {
+                    using (SqlCommand cmd = holder.Connection.CreateSPCommand("get_companies"))
+                    {
+                        cmd.Parameters.AddWithValue("@id_company", userId);
+                        try
+                        {
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                    res.Add(new Company(reader));
+                            }
+                        }
+                        catch (SqlException e)
+                        {
+                            throw e;
+                        }
+                    }
+                }
+                //save to cache
+                CacheWrapper.SetList<Company>(CacheKey, res, userId);
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Naviam.DAL/Entities/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace Naviam.Data
+{
+    public class Company: DbEntity
+    {
+        public Company()  {}
+
+        public Company(IDataRecord record)
+        {
+            Id = record["id"] as int?;
+            CountryId = record["id_country"] as int?;
+            BuinessName = record["name_business"] as string;
+            TypeId = record["id_company_type"] as int?;
+        }
+
+        public int? CountryId { get; set; }
+        public string BuinessName { get; set; }
+        public int? TypeId { get; set; }
+    }
+}
