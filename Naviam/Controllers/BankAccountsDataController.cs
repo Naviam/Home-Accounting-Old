b62f0b038b65fac185e135712d7413f85bb97b40
Vitali Hatalski <v.hatalski@gmail.com>
2011-08-11 15:20:45 +0300

    namespaces refactored

--- Naviam/Controllers/BankAccountsDataController.cs
+++ Naviam/Controllers/BankAccountsDataController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace Naviam.Controllers
+namespace Naviam.WebUI.Controllers
 {
     public class BankAccountsDataController : BaseController
     {
