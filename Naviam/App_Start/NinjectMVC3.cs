a0af11e862d5768d2faddd8ba95479b4cdff066f
Vitali Hatalski <v.hatalski@gmail.com>
2011-08-11 15:22:11 +0300

    avoided for now

---
+++ Naviam/App_Start/NinjectMVC3.cs
@@ -0,0 +1,52 @@
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Naviam.WebUI.App_Start;
+using Ninject;
+using Ninject.Web.Mvc;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(NinjectMvc3), "Start")]
+[assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(NinjectMvc3), "Stop")]
+
+namespace Naviam.WebUI.App_Start
+{
+    public static class NinjectMvc3 
+    {
+        private static readonly Bootstrapper Bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestModule));
+            DynamicModuleUtility.RegisterModule(typeof(HttpApplicationInitializationModule));
+            Bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            Bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+        }        
+    }
+}
