6ef54e70823b7a6f5e686076d9f3b46ef8715301
Vitali Hatalski <v.hatalski@gmail.com>
2011-08-11 15:24:54 +0300

    refactored

--- TestParsers/Program.cs
+++ TestParsers/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Naviam.Code.Parsers;
+using Naviam.WebUI.Helpers.Parsers;
 
 namespace TestParsers
 {
