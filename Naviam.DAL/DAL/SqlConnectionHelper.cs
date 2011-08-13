16deeead420559d3065db85d356773c7b1f7fd5d
Vitali Hatalski <v.hatalski@gmail.com>
2011-08-13 12:43:05 +0300

    added ability to add details to sqlexception

--- Naviam.DAL/DAL/SqlConnectionHelper.cs
+++ Naviam.DAL/DAL/SqlConnectionHelper.cs
@@ -5,19 +5,43 @@ using System.Data.SqlClient;
 using System.Configuration;
 using System.Collections;
 using System.Data;
-using Npgsql;
+using System.Web.Hosting;
 
 namespace Naviam.DAL
 {
+    public class Global
+    {
+        public static string ProcNameExceptionDataKey = "procname";
+        public static string ParamExceptionDataKey = "parameters";
+    }
+
+    #region SqlCommandExtensions
+
+    public static class SqlCommandExtensions
+    {
+        public static void AddDetailsToException(this SqlCommand command, SqlException e)
+        {
+            var procedureName = String.Format("Procedure Name: {0}", command.CommandText);
+            e.Data[Global.ProcNameExceptionDataKey] = procedureName;
+            var parameters = String.Format("Passed parameters: {0}", Environment.NewLine);
+            foreach (SqlParameter sqlParam in command.Parameters)
+            {
+                var paramValue = sqlParam.Value != null ? sqlParam.Value.ToString() : "";
+                parameters = parameters + sqlParam.ParameterName + ": " + paramValue +
+                    "(" + sqlParam.Size.ToString() + "), ";
+            }
+            e.Data[Global.ParamExceptionDataKey] = parameters.Remove(parameters.Length - 2);
+        }
+    }
+
+    #endregion
+
     /// <summary>
     /// Provides access to SqlConnection Helper Methods
     /// </summary>
     public static class SqlConnectionHelper
     {
-        public enum ConnectionType
-        {
-            Naviam
-        }
+        public enum ConnectionType { Naviam }
 
         const string KeyDataSource = "connectionString.dataSource";
 
@@ -27,15 +51,15 @@ namespace Naviam.DAL
         #region METHOD::GetConnection
         public static SqlConnectionHolder GetConnection(ConnectionType connectionType = ConnectionType.Naviam)
         {
-            return GetConnection(GetConnString(connectionType));
+            return GetConnection(GetConnString(connectionType), false);
         }
-        public static SqlConnectionHolder GetConnection(string connectionString)
+        public static SqlConnectionHolder GetConnection(string connectionString, bool revertImpersonation)
         {
             var holder = new SqlConnectionHolder(connectionString);
             var closeConn = true;
             try
             {
-                holder.Open();
+                holder.Open(revertImpersonation);
                 closeConn = false;
             }
             finally
@@ -51,8 +75,7 @@ namespace Naviam.DAL
         #endregion
 
         #region METHOD::GetConnString
-
-        public static string GetConnString(ConnectionType connectionType)
+        public static string GetConnString(ConnectionType connectionType = ConnectionType.Naviam)
         {
             if (ConnectionStrings[connectionType] == null)
             {
@@ -75,52 +98,45 @@ namespace Naviam.DAL
     /// </summary>
     public sealed class SqlConnectionHolder : IDisposable
     {
-        private const string SqlErrorConnectionString = "An error occurred while attempting to initialize a SqlConnectionHolder object. The value that was provided for the connection string may be wrong, or it may contain an invalid syntax.";
+        private const string SqlErrorConnectionString = "An error occurred while attempting to initialize a System.Data.SqlClient.SqlConnection object. The value that was provided for the connection string may be wrong, or it may contain an invalid syntax.";
 
         private bool _opened;
-
-        #region PROPERTY::Connection
-
-#if _USE_MS_SQL 
+        
         public SqlConnection Connection { get; private set; }
-#else
-        public NpgsqlConnection Connection { get; private set; }
-#endif
 
-        #endregion
-
-        #region Constructor
         internal SqlConnectionHolder(string connectionString)
         {
             try
             {
-#if _USE_MS_SQL 
                 Connection = new SqlConnection(connectionString);
-#else
-                Connection = new NpgsqlConnection(connectionString);
-#endif
             }
             catch (ArgumentException e)
             {
                 throw new ArgumentException(SqlErrorConnectionString, "connectionString", e);
             }
         }
-        #endregion
-
-        #region METHOD::Open
-        internal void Open()
+        
+        public void Open(bool revertImpersonate)
         {
             if (_opened)
                 return; // Already opened
 
-            Connection.Open();
+            if (revertImpersonate)
+            {
+                using (HostingEnvironment.Impersonate())
+                {
+                    Connection.Open();
+                }
+            }
+            else
+            {
+                Connection.Open();
+            }
 
             _opened = true; // Open worked!
         }
-        #endregion
 
-        #region METHOD::Close
-        internal void Close()
+        public void Close()
         {
             if (!_opened) // Not open!
                 return;
@@ -128,7 +144,6 @@ namespace Naviam.DAL
             Connection.Close();
             _opened = false;
         }
-        #endregion
 
         public void Dispose()
         {
