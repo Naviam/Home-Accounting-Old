bc837b149d58c4656cbbc0eaf76f226479be33eb
Vitali Hatalski <v.hatalski@gmail.com>
2011-08-15 23:02:50 +0300

    Roles management classes

---
+++ Naviam.DAL/DAL/RoleDataAdapter.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Naviam.DAL
+{
+    public class RoleDataAdapter
+    {
+        public static int CommandTimeout { get; set; }
+
+        public static int CreateRole(string roleName)
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.role_create";
+
+                    cmd.Parameters.AddWithValue("@role_name", roleName);
+                    var returnParam = new SqlParameter("ReturnValue", SqlDbType.Int, 4)
+                                          {
+                                              Direction = ParameterDirection.ReturnValue
+                                          };
+                    cmd.Parameters.Add(returnParam);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        return (int)returnParam.Value;
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static int DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.role_del";
+
+                    cmd.Parameters.AddWithValue("@role_name", roleName);
+                    cmd.Parameters.AddWithValue("@delete_only_if_role_is_empty", throwOnPopulatedRole);
+                    var returnParam = new SqlParameter("ReturnValue", SqlDbType.Int, 4)
+                    {
+                        Direction = ParameterDirection.ReturnValue
+                    };
+                    cmd.Parameters.Add(returnParam);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        return (int)returnParam.Value;
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static string[] GetAllRoles()
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.roles_get";
+                    
+                    var roles = new List<string>();
+
+                    try
+                    {
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            roles.Add(reader.GetString(0));
+                        }
+                        return roles.ToArray();
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static int RoleExists(string roleName)
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.role_exists";
+
+                    cmd.Parameters.AddWithValue("@role_name", roleName);
+                    var returnParam = new SqlParameter("ReturnValue", SqlDbType.Int, 4)
+                    {
+                        Direction = ParameterDirection.ReturnValue
+                    };
+                    cmd.Parameters.Add(returnParam);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        return (int) returnParam.Value;
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static int IsUserInRole(string userName, string roleName)
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.is_user_in_role";
+
+                    cmd.Parameters.AddWithValue("@email", userName);
+                    cmd.Parameters.AddWithValue("@role_name", roleName);
+                    var returnParam = new SqlParameter("ReturnValue", SqlDbType.Int, 4)
+                    {
+                        Direction = ParameterDirection.ReturnValue
+                    };
+                    cmd.Parameters.Add(returnParam);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        return (int)returnParam.Value;
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static string[] GetUsersInRole(string roleName, out int resultValue)
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.users_in_roles_get_users_in_role";
+
+                    cmd.Parameters.AddWithValue("@role_name", roleName);
+                    var returnParam = new SqlParameter("ReturnValue", SqlDbType.Int, 4)
+                    {
+                        Direction = ParameterDirection.ReturnValue
+                    };
+                    cmd.Parameters.Add(returnParam);
+
+                    var users = new List<string>();
+                    try
+                    {
+                        var reader = cmd.ExecuteReader(CommandBehavior.SequentialAccess);
+                        while (reader.Read())
+                        {
+                            users.Add(reader.GetString(0));
+                        }
+                        resultValue = (int)returnParam.Value;
+                        return users.ToArray();
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static string[] GetRolesForUser(string userName, out int resultValue)
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.users_in_roles_get_roles_for_user";
+
+                    cmd.Parameters.AddWithValue("@email", userName);
+                    var returnParam = new SqlParameter("ReturnValue", SqlDbType.Int, 4)
+                    {
+                        Direction = ParameterDirection.ReturnValue
+                    };
+                    cmd.Parameters.Add(returnParam);
+
+                    var roles = new List<string>();
+                    try
+                    {
+                        var reader = cmd.ExecuteReader(CommandBehavior.SequentialAccess);
+                        while (reader.Read())
+                        {
+                            roles.Add(reader.GetString(0));
+                        }
+                        resultValue = (int)returnParam.Value;
+                        return roles.ToArray();
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static string[] FindUsersInRole(string roleName, string usernameToMatch, out int resultValue)
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.users_in_roles_find_users_in_role";
+
+                    cmd.Parameters.AddWithValue("@role_name", roleName);
+                    cmd.Parameters.AddWithValue("@user_name_to_match", usernameToMatch);
+                    var returnParam = new SqlParameter("ReturnValue", SqlDbType.Int, 4)
+                    {
+                        Direction = ParameterDirection.ReturnValue
+                    };
+                    cmd.Parameters.Add(returnParam);
+
+                    var roles = new List<string>();
+                    try
+                    {
+                        var reader = cmd.ExecuteReader(CommandBehavior.SequentialAccess);
+                        while (reader.Read())
+                        {
+                            roles.Add(reader.GetString(0));
+                        }
+                        resultValue = (int)returnParam.Value;
+                        return roles.ToArray();
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static string[] AddUsersToRoles(string userNames, string roleNames, out int resultValue)
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.users_in_roles_add_users_to_roles";
+
+                    cmd.Parameters.AddWithValue("@emails", userNames);
+                    cmd.Parameters.AddWithValue("@roles", roleNames);
+                    cmd.Parameters.AddWithValue("@current_time_utc", DateTime.UtcNow);
+                    var returnParam = new SqlParameter("ReturnValue", SqlDbType.Int, 4)
+                    {
+                        Direction = ParameterDirection.ReturnValue
+                    };
+                    cmd.Parameters.Add(returnParam);
+
+                    var roles = new List<string>();
+                    try
+                    {
+                        var reader = cmd.ExecuteReader(CommandBehavior.SingleRow);
+                        while (reader.Read())
+                        {
+                            roles.Add(reader.GetString(0));
+                        }
+                        resultValue = (int)returnParam.Value;
+                        return roles.ToArray();
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static string[] RemoveUsersFromRoles(string userNames, string roleNames, out int resultValue)
+        {
+            using (var connection = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = connection.Connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = CommandTimeout;
+                    cmd.CommandText = "web.users_in_roles_add_users_to_roles";
+
+                    cmd.Parameters.AddWithValue("@emails", userNames);
+                    cmd.Parameters.AddWithValue("@roles", roleNames);
+                    cmd.Parameters.AddWithValue("@current_time_utc", DateTime.UtcNow);
+                    var returnParam = new SqlParameter("ReturnValue", SqlDbType.Int, 4)
+                    {
+                        Direction = ParameterDirection.ReturnValue
+                    };
+                    cmd.Parameters.Add(returnParam);
+
+                    var roles = new List<string>();
+                    try
+                    {
+                        var reader = cmd.ExecuteReader(CommandBehavior.SingleRow);
+                        while (reader.Read())
+                        {
+                            roles.Add(reader.GetString(0));
+                        }
+                        resultValue = (int)returnParam.Value;
+                        return roles.ToArray();
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Naviam.Domain/Concrete/RoleRepository.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Configuration.Provider;
+using Naviam.DAL;
+using Naviam.Domain.Entities;
+
+namespace Naviam.Domain.Concrete
+{
+    public class RoleRepository
+    {
+        private static RoleRepository _instance;
+
+        private RoleRepository()
+        {
+        }
+
+        public static RoleRepository GetInstance()
+        {
+            return _instance ?? (_instance = new RoleRepository());
+        }
+
+        public void CreateRole(string roleName)
+        {
+            SecUtility.CheckParameter(ref roleName, true, true, true, 0x100, "roleName");
+
+            var returnResult = RoleDataAdapter.CreateRole(roleName);
+
+            switch (returnResult)
+            {
+                case 0: return;
+                case 1: throw new ProviderException("Role already exists");
+            }
+            throw new ProviderException("Unknown failure");
+        }
+
+        public bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            SecUtility.CheckParameter(ref roleName, true, true, true, 0x100, "roleName");
+            var returnValue = RoleDataAdapter.DeleteRole(roleName, throwOnPopulatedRole);
+
+            if (returnValue == 2)
+            {
+                throw new ProviderException("Role is not empty");
+            }
+
+            return returnValue == 0;
+        }
+
+        public string[] GetAllRoles()
+        {
+            return RoleDataAdapter.GetAllRoles();
+        }
+
+        public string[] GetRolesForUser(string username)
+        {
+            SecUtility.CheckParameter(ref username, true, false, true, 0x100, "username");
+            if (username.Length < 1)
+            {
+                return new string[0];
+            }
+            int returnValue;
+            var roles = RoleDataAdapter.GetRolesForUser(username, out returnValue);
+
+            if (roles.Length > 0)
+            {
+                return roles;
+            }
+            switch (returnValue)
+            {
+                case 0: return new string[0];
+                case 1: return new string[0];
+            }
+            throw new ProviderException("Unknown Failure");
+        }
+
+        public string[] GetUsersInRole(string roleName)
+        {
+            SecUtility.CheckParameter(ref roleName, true, true, true, 0x100, "roleName");
+
+            int returnValue;
+            var users = RoleDataAdapter.GetUsersInRole(roleName, out returnValue);
+            if (users.Length < 1)
+            {
+                switch (returnValue)
+                {
+                    case 0: return new string[0];
+                    case 1: throw new ProviderException("Role has not been found");
+                }
+                throw new ProviderException("Unknown Failure");
+            }
+            return users;
+        }
+
+        public bool IsUserInRole(string username, string roleName)
+        {
+            SecUtility.CheckParameter(ref roleName, true, true, true, 0x100, "roleName");
+            SecUtility.CheckParameter(ref username, true, false, true, 0x100, "username");
+            if (username.Length < 1)
+            {
+                return false;
+            }
+            var resultValue = RoleDataAdapter.IsUserInRole(username, roleName);
+            switch (resultValue)
+            {
+                case 0: return false;
+                case 1: return true;
+                case 2: return false;
+                case 3: return false;
+            }
+            throw new ProviderException("Unknown Failure");
+        }
+
+        public string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            SecUtility.CheckParameter(ref roleName, true, true, true, 0x100, "roleName");
+            SecUtility.CheckParameter(ref usernameToMatch, true, true, false, 0x100, "usernameToMatch");
+
+            int returnValue;
+            var users = RoleDataAdapter.FindUsersInRole(roleName, usernameToMatch, out returnValue);
+
+            if (users.Length < 1)
+            {
+                switch (returnValue)
+                {
+                    case 0: return new string[0];
+                    case 1: throw new ProviderException("Role has not been found");
+                }
+                throw new ProviderException("Unknown failure");
+            }
+
+            return users;
+        }
+
+        public void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            SecUtility.CheckArrayParameter(ref roleNames, true, true, true, 0x100, "roleNames");
+            SecUtility.CheckArrayParameter(ref usernames, true, true, true, 0x100, "usernames");
+            int resultValue;
+            RoleDataAdapter.AddUsersToRoles(String.Join(",", usernames), String.Join(",", roleNames), out resultValue);
+
+            switch (resultValue)
+            {
+                case 0: return;
+                case 1: throw new ProviderException("This user has not been found");
+                case 2: throw new ProviderException("Role has not been found");
+                case 3: throw new ProviderException("This user is already in role");
+            }
+        }
+
+        public void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            SecUtility.CheckArrayParameter(ref roleNames, true, true, true, 0x100, "roleNames");
+            SecUtility.CheckArrayParameter(ref usernames, true, true, true, 0x100, "usernames");
+            int resultValue;
+            RoleDataAdapter.RemoveUsersFromRoles(String.Join(",", usernames), String.Join(",", roleNames), out resultValue);
+
+            switch (resultValue)
+            {
+                case 0: return;
+                case 1: throw new ProviderException("This user has not been found");
+                case 2: throw new ProviderException("Role has not been found");
+                case 3: throw new ProviderException("This user is not in role");
+            }
+            throw new ProviderException("Unknown failure");
+        }
+
+        public bool RoleExists(string roleName)
+        {
+            SecUtility.CheckParameter(ref roleName, true, true, true, 0x100, "roleName");
+            var returnValue = RoleDataAdapter.RoleExists(roleName);
+
+            switch (returnValue)
+            {
+                case 0: return false;
+                case 1: return true;
+            }
+            throw new ProviderException("Unknown failure");
+        }
+    }
+}
