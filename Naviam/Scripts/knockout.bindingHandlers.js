fc7c72eeff009dd88b8cff209e7f0f156644498a
PavelMironchik <pavel.mironchik@gmail.com>
2011-08-15 14:03:25 +0300

    update naviam

---
+++ Naviam/Scripts/knockout.bindingHandlers.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="jquery-1.6.2.js" />
+/// <reference path="knockout-1.2.1.js" />
+
+ko.bindingHandlers.msDateTime = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+    },
+    update: function (element, valueAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        if (value != null) {
+            var date = eval("new " + value.replace(/\//g, ''));
+            var val = date.format();
+            $(element).text(val);
+        }
+    }
+};
