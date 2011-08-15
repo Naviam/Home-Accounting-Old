a095b9f2728c9b59b07e8c0c37fcf284e0d01aee
Vitali Hatalski <v.hatalski@gmail.com>
2011-08-15 23:05:07 +0300

    static was removed

--- Naviam.DAL/CacheWrapper.cs
+++ Naviam.DAL/CacheWrapper.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.SessionState;
 using System.Configuration;
 using ServiceStack.Redis;
 using System.Web.WebPages;
 using System.Web.Security;
 
-namespace Naviam.WebUI
+namespace Naviam
 {
-    public class CacheWrapper
+    public interface ICacheWrapper
     {
+        T Get<T>(string key);
+        T Get<T>(string key, params int?[] id);
+        T GetAndSet<T>(string key, T val);
+        T GetAndSet<T>(string key, T val, params int?[] id);
+        void ProlongKey(string key);
+        void Set<T>(string key, T val);
+        void Set<T>(string key, T val, bool forceExpire, params int?[] id);
+        List<T> GetList<T>(string key);
+        List<T> GetList<T>(string key, params int?[] id);
+        void SetList<T>(string key, List<T> val);
+        void SetList<T>(string key, List<T> val, params int?[] id);
+        void AddToList<T>(string key, T val);
+        void AddToList<T>(string key, T val, params int?[] id);
+        void UpdateList<T>(string key, T val);
+        void UpdateList<T>(string key, T val, params int?[] id);
+        void RemoveFromList<T>(string key, T val);
+        void RemoveFromList<T>(string key, T val, params int?[] id);
+        T GetFromList<T>(string key, T val);
+        T GetFromList<T>(string key, T val, params int?[] id);
+    }
 
+    public class CacheWrapper : ICacheWrapper
+    {
         //Tests:
         //Load of 300000    Save of 300000
         //simple-4900ms     simple-3200ms
@@ -20,10 +41,10 @@ namespace Naviam.WebUI
 
         #region Single obj
 
-        public static T Get<T>(string key) { return Get<T>(key, null); }
-        public static T Get<T>(string key, params int?[] id)
+        public T Get<T>(string key) { return Get<T>(key, null); }
+        public T Get<T>(string key, params int?[] id)
         {
-            T res = default(T);
+            var res = default(T);
             key = GetKey(key, id);
             if (ConfigurationManager.AppSettings["EnableRedis"].AsBool())
             {
@@ -35,25 +56,17 @@ namespace Naviam.WebUI
             }
             else
             {
-                object obj = null;
-                if (id != null)
-                {
-                    obj = HttpContext.Current.Session[key];
-                }
-                else
-                {
-                    obj = HttpContext.Current.Cache[key];
-                }
+                var obj = id != null ? HttpContext.Current.Session[key] : HttpContext.Current.Cache[key];
                 if (obj != null)
                     res = (T)obj;
             }
             return res;
         }
 
-        public static T GetAndSet<T>(string key, T val) { return GetAndSet<T>(key, val, null); }
-        public static T GetAndSet<T>(string key, T val, params int?[] id)
+        public T GetAndSet<T>(string key, T val) { return GetAndSet(key, val, null); }
+        public T GetAndSet<T>(string key, T val, params int?[] id)
         {
-            T res = default(T);
+            var res = default(T);
             key = GetKey(key, id);
             if (ConfigurationManager.AppSettings["EnableRedis"].AsBool())
             {
@@ -64,7 +77,7 @@ namespace Naviam.WebUI
                     if (id != null)
                     {
                         //session style
-                        TimeSpan exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
+                        var exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
                         using (redisClient.AcquireLock(key + "lock"))
                         {
                             if (val == null)
@@ -85,7 +98,7 @@ namespace Naviam.WebUI
             }
             else
             {
-                object obj = null;
+                object obj;
                 if (id != null)
                 {
                     obj = HttpContext.Current.Session[key];
@@ -102,20 +115,20 @@ namespace Naviam.WebUI
             return res;
         }
 
-        public static void ProlongKey(string key)
+        public void ProlongKey(string key)
         {
             if (ConfigurationManager.AppSettings["EnableRedis"].AsBool())
             {
                 using (var redisClient = new RedisClient(ConfigurationManager.AppSettings["RedisHost"], Convert.ToInt32(ConfigurationManager.AppSettings["RedisPort"])))
                 {
-                    TimeSpan exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
+                    var exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
                     redisClient.ExpireEntryIn(key, exp);
                 }
             }
         }
 
-        public static void Set<T>(string key, T val) { Set<T>(key, val, false, null); }
-        public static void Set<T>(string key, T val, bool forceExpire, params int?[] id)
+        public void Set<T>(string key, T val) { Set(key, val, false, null); }
+        public void Set<T>(string key, T val, bool forceExpire, params int?[] id)
         {
             key = GetKey(key, id);
             if (ConfigurationManager.AppSettings["EnableRedis"].AsBool())
@@ -126,7 +139,7 @@ namespace Naviam.WebUI
                     if (id != null || forceExpire)
                     {
                         //session style
-                        TimeSpan exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
+                        var exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
                         using (redisClient.AcquireLock(key + "lock"))
                         {
                             if (val == null)
@@ -168,8 +181,8 @@ namespace Naviam.WebUI
 
         #region Lists
 
-        public static List<T> GetList<T>(string key) { return GetList<T>(key, null); }
-        public static List<T> GetList<T>(string key, params int?[] id)
+        public List<T> GetList<T>(string key) { return GetList<T>(key, null); }
+        public List<T> GetList<T>(string key, params int?[] id)
         {
             List<T> res = null;
             key = GetKey(key, id);
@@ -186,23 +199,15 @@ namespace Naviam.WebUI
             }
             else
             {
-                object obj = null;
-                if (id != null)
-                {
-                    obj = HttpContext.Current.Session[key];
-                }
-                else
-                {
-                    obj = HttpContext.Current.Cache[key];
-                }
+                var obj = id != null ? HttpContext.Current.Session[key] : HttpContext.Current.Cache[key];
                 if (obj != null)
                     res = (List<T>)obj;
             }
             return res;
         }
 
-        public static void SetList<T>(string key, List<T> val) { SetList<T>(key, val, null); }
-        public static void SetList<T>(string key, List<T> val, params int?[] id)
+        public void SetList<T>(string key, List<T> val) { SetList(key, val, null); }
+        public void SetList<T>(string key, List<T> val, params int?[] id)
         {
             key = GetKey(key, id);
             if (ConfigurationManager.AppSettings["EnableRedis"].AsBool())
@@ -215,12 +220,12 @@ namespace Naviam.WebUI
                     {
                         var list = typedRedis.Lists[key];
                         list.Clear();
-                        val.ForEach(x => list.Add(x));
+                        val.ForEach(list.Add);
                     }
                     if (id != null)
                     {
                         //session style
-                        TimeSpan exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
+                        var exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
                         typedRedis.ExpireEntryIn(key, exp);
                     }
                 }
@@ -234,8 +239,8 @@ namespace Naviam.WebUI
             }
         }
 
-        public static void AddToList<T>(string key, T val) { AddToList<T>(key, val, null); }
-        public static void AddToList<T>(string key, T val, params int?[] id)
+        public void AddToList<T>(string key, T val) { AddToList(key, val, null); }
+        public void AddToList<T>(string key, T val, params int?[] id)
         {
             key = GetKey(key, id);
             if (ConfigurationManager.AppSettings["EnableRedis"].AsBool())
@@ -252,18 +257,14 @@ namespace Naviam.WebUI
                     if (id != null)
                     {
                         //session style
-                        TimeSpan exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
+                        var exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
                         typedRedis.ExpireEntryIn(key, exp);
                     }
                 }
             }
             else
             {
-                object obj = null;
-                if (id != null)
-                    obj = HttpContext.Current.Session[key];
-                else
-                    obj = HttpContext.Current.Cache[key];
+                var obj = id != null ? HttpContext.Current.Session[key] : HttpContext.Current.Cache[key];
                 if (obj != null)
                 {
                     ((List<T>)obj).Add(val);
@@ -271,8 +272,8 @@ namespace Naviam.WebUI
             }
         }
 
-        public static void UpdateList<T>(string key, T val) { UpdateList<T>(key, val, null); }
-        public static void UpdateList<T>(string key, T val, params int?[] id)
+        public void UpdateList<T>(string key, T val) { UpdateList(key, val, null); }
+        public void UpdateList<T>(string key, T val, params int?[] id)
         {
             key = GetKey(key, id);
             if (ConfigurationManager.AppSettings["EnableRedis"].AsBool())
@@ -284,29 +285,25 @@ namespace Naviam.WebUI
                     using (redisClient.AcquireLock(key + "lock"))
                     {
                         var list = typedRedis.Lists[key];
-                        int index = list.IndexOf(val);
+                        var index = list.IndexOf(val);
                         if (index != -1)
                             list[index] = val;
                     }
                     if (id != null)
                     {
                         //session style
-                        TimeSpan exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
+                        var exp = new TimeSpan(0, (int)FormsAuthentication.Timeout.TotalMinutes, 0);
                         typedRedis.ExpireEntryIn(key, exp);
                     }
                 }
             }
             else
             {
-                object obj = null;
-                if (id != null)
-                    obj = HttpContext.Current.Session[key];
-                else
-                    obj = HttpContext.Current.Cache[key];
+                var obj = id != null ? HttpContext.Current.Session[key] : HttpContext.Current.Cache[key];
                 if (obj != null)
                 {
-                    List<T> lst = (List<T>)obj;
-                    for (int i = 0; i < lst.Count; i++)
+                    var lst = (List<T>)obj;
+                    for (var i = 0; i < lst.Count; i++)
                     {
                         var existingItem = lst[i];
                         if (existingItem.Equals(val))
@@ -319,8 +316,8 @@ namespace Naviam.WebUI
             }
         }
 
-        public static void RemoveFromList<T>(string key, T val) { RemoveFromList<T>(key, val, null); }
-        public static void RemoveFromList<T>(string key, T val, params int?[] id)
+        public void RemoveFromList<T>(string key, T val) { RemoveFromList(key, val, null); }
+        public void RemoveFromList<T>(string key, T val, params int?[] id)
         {
             key = GetKey(key, id);
             if (ConfigurationManager.AppSettings["EnableRedis"].AsBool())
@@ -338,23 +335,19 @@ namespace Naviam.WebUI
             }
             else
             {
-                object obj = null;
-                if (id != null)
-                    obj = HttpContext.Current.Session[key];
-                else
-                    obj = HttpContext.Current.Cache[key];
+                var obj = id != null ? HttpContext.Current.Session[key] : HttpContext.Current.Cache[key];
                 if (obj != null)
                 {
-                    List<T> lst = (List<T>)obj;
+                    var lst = (List<T>)obj;
                     lst.Remove(val);
                 }
             }
         }
 
-        public static T GetFromList<T>(string key, T val) { return GetFromList<T>(key, val, null); }
-        public static T GetFromList<T>(string key, T val, params int?[] id)
+        public T GetFromList<T>(string key, T val) { return GetFromList(key, val, null); }
+        public T GetFromList<T>(string key, T val, params int?[] id)
         {
-            T res = default(T);
+            var res = default(T);
             key = GetKey(key, id);
             if (ConfigurationManager.AppSettings["EnableRedis"].AsBool())
             {
@@ -363,29 +356,21 @@ namespace Naviam.WebUI
                     var typedRedis = redisClient.GetTypedClient<T>();
                    
                     var list = typedRedis.Lists[key];
-                    int index = list.IndexOf(val);
+                    var index = list.IndexOf(val);
                     if (index != -1)
                         res = list[index];
                 }
             }
             else
             {
-                object obj = null;
-                if (id != null)
-                    obj = HttpContext.Current.Session[key];
-                else
-                    obj = HttpContext.Current.Cache[key];
+                var obj = id != null ? HttpContext.Current.Session[key] : HttpContext.Current.Cache[key];
                 if (obj != null)
                 {
-                    List<T> lst = (List<T>)obj;
-                    for (int i = 0; i < lst.Count; i++)
+                    var lst = (List<T>)obj;
+                    foreach (var t in from t in lst let existingItem = t where existingItem.Equals(val) select t)
                     {
-                        var existingItem = lst[i];
-                        if (existingItem.Equals(val))
-                        {
-                            res = lst[i];
-                            break;
-                        }
+                        res = t;
+                        break;
                     }
                 }
             }
@@ -397,11 +382,7 @@ namespace Naviam.WebUI
         {
             if (id != null)
             {
-                for (int i = 0; i < id.Length; i++)
-                {
-                    if (id[i].HasValue)
-                        key = key + "_" + id[i].Value;
-                }
+                key = id.Where(t => t.HasValue).Aggregate(key, (current, t) => t != null ? current + "_" + t.Value : null);
             }
             return key;
         }
