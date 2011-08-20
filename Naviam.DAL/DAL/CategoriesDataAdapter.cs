8c19c2a42ebc0311bf141063be3d3cb87c885363
PavelMironchik <pavel.mironchik@gmail.com>
2011-08-20 17:54:09 +0300

    move' get categories' to repository

--- Naviam.DAL/DAL/CategoriesDataAdapter.cs
+++ Naviam.DAL/DAL/CategoriesDataAdapter.cs
@@ -6,40 +6,30 @@ namespace Naviam.DAL
 {
     public class CategoriesDataAdapter
     {
-        private const string CacheKey = "transCategory";
-
-        public static List<Category> GetCategories(int? userId) { return GetCategories(userId, false); }
-        public static List<Category> GetCategories(int? userId, bool forceSqlLoad)
+        public static List<Category> GetCategories(int? userId)
         {
-            var cache = new CacheWrapper();
-            var res = cache.GetList<Category>(CacheKey, userId);
-            if (res == null || forceSqlLoad)
+            var res = new List<Category>();
+            using (var holder = SqlConnectionHelper.GetConnection())
             {
-                //load from DB
-                res = new List<Category>();
-                using (var holder = SqlConnectionHelper.GetConnection())
+                using (var cmd = holder.Connection.CreateSPCommand("categories_get"))
                 {
-                    using (var cmd = holder.Connection.CreateSPCommand("categories_get"))
+                    cmd.Parameters.AddWithValue("@id_user", userId);
+                    try
                     {
-                        cmd.Parameters.AddWithValue("@id_user", userId);
-                        try
+                        using (var reader = cmd.ExecuteReader())
                         {
-                            using (var reader = cmd.ExecuteReader())
-                            {
-                                res = new Categories(reader);
-                            }
-                        }
-                        catch (SqlException e)
-                        {
-                            cmd.AddDetailsToException(e);
-                            throw;
+                            res = new Categories(reader);
                         }
                     }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
                 }
-                //save to cache
-                cache.SetList(CacheKey, res, userId);
             }
             return res;
         }
     }
+
 }
