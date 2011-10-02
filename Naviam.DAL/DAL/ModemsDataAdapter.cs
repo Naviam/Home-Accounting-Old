539405e4bc3a9d7f1ed91f5f74ee0e50859dd93d
PavelMironchik <pavel.mironchik@gmail.com>
2011-10-02 16:46:15 +0300

    add modems data adapter

---
+++ Naviam.DAL/DAL/ModemsDataAdapter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Naviam.Data;
+using System.Data.SqlClient;
+
+namespace Naviam.DAL
+{
+    public class ModemsDataAdapter
+    {
+        public static List<Modem> GetModems(int? companyId)
+        {
+            List<Modem> res = new List<Modem>();
+            using (var holder = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = holder.Connection.CreateSPCommand("web.modems_get"))
+                {
+                    try
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                                res.Add(new Modem(reader));
+                        }
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+            return res;
+        }
+
+        public static Modem GetModemByGateway(string gateway)
+        {
+            Modem res = new Modem();
+            using (var holder = SqlConnectionHelper.GetConnection())
+            {
+                using (var cmd = holder.Connection.CreateSPCommand("web.modems_get"))
+                {
+                    cmd.Parameters.AddWithValue("@gateway", gateway.ToDbValue());
+                    try
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                                res=new Modem(reader);
+                        }
+                    }
+                    catch (SqlException e)
+                    {
+                        cmd.AddDetailsToException(e);
+                        throw;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Naviam.DAL/Entities/Modem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Naviam.Data
+{
+    [Serializable]
+    public class Modem : DbEntity
+    {
+        public Modem()
+        {
+
+        }
+        public Modem(SqlDataReader reader)
+        {
+            Id = reader["id"] as int?;
+            Description = reader["description"] as string;
+            Gateway = reader["gateway"] as string;
+        }
+
+        public string Description { get; set; }
+        public string Gateway { get; set; }
+    }//calass Modem
+
+    public static partial class SqlCommandExtensions
+    {
+        /// <summary>
+        /// Appends Account-specific parameters to the specificied SqlCommand. 
+        /// </summary>
+        /// <param name="command">SqlCommand to be executed.</param>
+        /// <param name="alert">Instance of Modem class</param>
+        /// <param name="action">Database action type (select, insert, update, delete).</param>
+        public static void AddEntityParameters(this SqlCommand command, Modem account, DbActionType action)
+        {
+            command.AddCommonParameters(account.Id, action);
+            command.Parameters.Add("@description", SqlDbType.NVarChar).Value = account.Description.ToDbValue();
+            command.Parameters.Add("@gateway", SqlDbType.NVarChar).Value = account.Description.ToDbValue();
+        }
+    }
+
+}
