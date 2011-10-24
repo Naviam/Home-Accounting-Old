8a8f1172fca05ee2598ed4c8f7b3c7ada32373af
PavelMironchik <pavel.mironchik@gmail.com>
2011-10-24 15:40:29 +0300

    add category rule entity

---
+++ Naviam.DAL/Entities/CategoryRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Naviam.Data
+{
+    public class CategoryRule: DbEntity
+    {
+        public CategoryRule()
+        {
+
+        }
+        public CategoryRule(SqlDataReader reader)
+        {
+            Id = reader["id"] as int?;
+            CategoryId = reader["category_id"] as int?;
+            Priority= reader["priority"] as int?;
+            RegX = reader["regex"] as string;
+        }
+
+        public int? CategoryId { get; set; }
+        public int? Priority { get; set; }
+        public string RegX { get; set; }
+    }
+}
