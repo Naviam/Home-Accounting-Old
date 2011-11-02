0b4c206595812e90a21edb6ce2dcaba002dbb420
Aleksey Yurkevich <BlackTrinitron@gmail.com>
2011-11-02 23:23:09 +0300

    better table view

--- Naviam/Controllers/ReportsController.cs
+++ Naviam/Controllers/ReportsController.cs
@@ -58,33 +58,47 @@ namespace Naviam.WebUI.Controllers
             request.selectedMenu = request.selectedMenu ?? 0;
             request.selectedSubMenu = request.selectedSubMenu ?? 0;
 
+            var headColumn1 = Resources.JavaScript.Category;
+            var headColumn2 = Resources.JavaScript.Spending;
+
             if ((request.selectedMenu == 0 || request.selectedMenu == 1))
             {
+                if (request.selectedMenu == 1)
+                    headColumn2 = Resources.JavaScript.Income;
                 if (request.selectedSubMenu == 0)
+                {
+                    headColumn1 = Resources.JavaScript.Category;
                     report = from t in trans
                              join c in cats on t.CategoryId equals c.Id
                              where t.Direction == (TransactionDirections)request.selectedMenu && t.CurrencyId == request.selectedCurrency
                              group t.Amount by c into g
                              orderby g.Sum() descending
                              select new { Id = g.Key.Id, Amount = g.Sum(), Name = g.Key.Name }; //
+                }
 
                 if (request.selectedSubMenu == 1)
+                {
+                    headColumn1 = Resources.JavaScript.Merchant;
                     report = from t in trans
                              where t.Direction == (TransactionDirections)request.selectedMenu && t.CurrencyId == request.selectedCurrency
                              group t by t.Description into g
                              orderby g.Sum(t => t.Amount) descending
                              select new { Id = g.Min(t => t.Id), Amount = g.Sum(t => t.Amount), Name = g.Key }; //
+                }
 
                 if (request.selectedSubMenu == 2)
+                {
+                    headColumn1 = Resources.JavaScript.Tag;
                     report = from t in trans
                              from tg in tags
                              where t.Direction == (TransactionDirections)request.selectedMenu && t.CurrencyId == request.selectedCurrency && t.TagIds.Contains(tg.Id.ToString())
                              group t.Amount by tg into g
                              orderby g.Sum() descending
                              select new { Id = g.Key.Id, Amount = g.Sum(), Name = g.Key.Name }; //
+                }
             }
 
-            return Json(new { items = report, currencies, request });
+            return Json(new { items = report, currencies, request, headColumn1, headColumn2 });
         }
 
     }
