378784f2bd246ee4509f7dd42c8006704fbb2c25
Vitali Hatalski <v.hatalski@gmail.com>
2011-12-23 16:12:33 +0300

    adding tests for cards info retrieval

---
+++ Naviam.Specs/InternetBank/SbsBankCardsInfoTestSuite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Naviam.InternetBank;
+
+namespace Naviam.Specs.InternetBank
+{
+    [TestFixture]
+    public class SbsBankCardsInfoTestSuite
+    {
+        private BankClient _client;
+
+        [SetUp]
+        public void Init()
+        {
+            _client = new BankClient(15);
+            const string userName = "FQ529";
+            const string password = "XUI4K";
+            var authenticated = _client.Login(userName, password);
+        }
+
+        [TestCase]
+        public void GetPaymentCards()
+        {
+            // arrange
+
+            // act
+            var cards = _client.GetPaymentCards();
+
+            // assert
+            Assert.IsNotNull(cards);
+            Assert.IsTrue(cards.Any());
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+            _client.Logout(true);
+        }
+    }
+}
---
+++ Naviam.Specs/InternetBank/SbsBankLoginTestSuite.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Naviam.InternetBank;
+
+namespace Naviam.Specs.InternetBank
+{
+    [TestFixture]
+    public class SbsBankLoginTestSuite
+    {
+        private BankClient _client;
+
+        [SetUp]
+        public void Init()
+        {
+            _client = new BankClient(15);
+        }
+
+        [TestCase]
+        public void LoginIncorrectCredentials()
+        {
+            // arrange
+            const string userName = "test";
+            const string password = "test";
+
+            // act
+            var authenticated = _client.Login(userName, password);
+
+            // assert
+            Assert.False(authenticated.IsAuthenticated);
+        }
+
+        [TestCase]
+        public void LoginCorrectCredentials()
+        {
+            // arrange
+            const string userName = "FQ529";
+            const string password = "XUI4K";
+
+            // act
+            var authenticated = _client.Login(userName, password);
+
+            // assert
+            Assert.True(authenticated.IsAuthenticated);
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+            _client.Logout(true);
+        }
+    }
+}
