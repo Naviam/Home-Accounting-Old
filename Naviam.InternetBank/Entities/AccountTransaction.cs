d78ac486ab711e9ad894f70048069cc4568c6c53
Vitali Hatalski <v.hatalski@gmail.com>
2012-01-08 15:35:08 +0300

    for easy data comparison ToString is overloaded

--- Naviam.InternetBank/Entities/AccountTransaction.cs
+++ Naviam.InternetBank/Entities/AccountTransaction.cs
@@ -15,6 +15,12 @@ namespace Naviam.InternetBank.Entities
         public decimal Commission { get; set; }
         public decimal TransactionAmount { get; set; }
 
+        public override string ToString()
+        {
+            return String.Format("Operation Date: {0}, Description: {1}, Operation Amount: {2}, Currency: {3}, Status: {4}, Transaction Date: {5}, Commission: {6}, Transaction Amount: {7}",
+                OperationDate.Date, OperationDescription, OperationAmount, Currency, Status, TransactionDate.Date, Commission, TransactionAmount);
+        }
+
         public string Card { get; set; }
         public string Region { get; set; }
         public string Place { get; set; }
