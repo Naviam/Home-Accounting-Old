bc7008a2ca6c5a22e1b8fba25fd92b8537712b3c
Vitali Hatalski <v.hatalski@gmail.com>
2012-01-08 05:00:54 +0300

    modified accounttransaction class so it has all necessary fields to parse transactions from internet bank to

--- Naviam.InternetBank/Entities/AccountTransaction.cs
+++ Naviam.InternetBank/Entities/AccountTransaction.cs
@@ -4,16 +4,21 @@ namespace Naviam.InternetBank.Entities
 {
     public class AccountTransaction
     {
+        // operation data
         public DateTime OperationDate { get; set; }
-        public DateTime TransactionDate { get; set; }
-        public string Card { get; set; }
         public string OperationDescription { get; set; }
+        public decimal OperationAmount { get; set; }
+        public string Currency { get; set; }
+        public string Status { get; set; }
+        // transaction data
+        public DateTime TransactionDate { get; set; }
+        public decimal Commission { get; set; }
+        public decimal TransactionAmount { get; set; }
+
+        public string Card { get; set; }
         public string Region { get; set; }
         public string Place { get; set; }
-        public string Currency { get; set; }
-        public decimal TransactionAmount { get; set; }
         public decimal AccountAmount { get; set; }
         public decimal Balance { get; set; }
-        public string Status { get; set; }
     }
 }
